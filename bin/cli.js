de1b91a84a7b927e027105a7c2bc0b05a7cca89f
Stanislav Sysoev <d4rkr00t@gmail.com>
2016-05-01 13:41:41 +0300

    feat(package): Update babel to v6

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,6 @@
 #! /usr/bin/env node
 'use strict';
 
-var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } }; })();
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
 var _fs = require('fs');
 
 var _fs2 = _interopRequireDefault(_fs);
@@ -13,18 +9,17 @@ var _lib = require('./../lib/');
 
 var _lib2 = _interopRequireDefault(_lib);
 
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 var args = process.argv.slice(2);
 var usage = 'Usage: subject status color [-o output.svg]';
-var badgs = new _lib2['default']();
+var badgs = new _lib2.default();
 
-var _args = _slicedToArray(args, 5);
-
-var subject = _args[0];
-var status = _args[1];
-var color = _args[2];
-var o = _args[3];
-var output = _args[4];
-// eslint-disable-line
+var subject = args[0];
+var status = args[1];
+var color = args[2];
+var o = args[3];
+var output = args[4]; // eslint-disable-line
 
 if (subject && status && color) {
   (function () {
@@ -35,7 +30,7 @@ if (subject && status && color) {
 
     var filename = output || subject + '-' + status + '-' + color + '.svg';
 
-    _fs2['default'].writeFile(filename, badgs.render(subject, status, color), 'utf-8', function () {
+    _fs2.default.writeFile(filename, badgs.render(subject, status, color), 'utf-8', function () {
       console.log('Badge created: ' + filename); // eslint-disable-line
 
       process.exit(0); // eslint-disable-line
